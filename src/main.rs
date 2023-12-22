c84defef8ac6566eae22d3e72c9dd36175e71a38
luweglarz <lucasweglarz9@gmail.com>
2023-12-22 15:33:54 +0100

    get_keys imperativ styl

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
-use std::fs;
+mod args;
+
+use std::collections;
+use std::collections::HashMap;
 use std::env;
 
-fn cli_args(args: env::Args) -> Vec<String>{
-    return if args.len() > 2 || args.len() < 2 {
-        println!("ft_ality should take 1 argument");
-        std::process::exit(1);
+use crate::args::ality_args;
+
+struct State {
+    key : char,
+    name : String,
+    transition: Vec<State>
+}
+
+//Should be in a functional way
+fn get_keys(keys_vec: Vec<&str>) -> collections::HashMap<char, String>{
+    let mut key_map = collections::HashMap::<char, String>::new();
+    for str in keys_vec{
+        let splitted: Vec<&str> = str.split("=").into_iter().map(|str| str.trim()).collect();
+        let split_str: (char, &str) = if splitted.len() == 2 && splitted[0].len() == 1 {
+            (splitted[0].chars().next().unwrap(), splitted[1])
+        }else {
+            eprintln!("Error reading file: wrong format",);
+            std::process::exit(1);
+        };
+        key_map.insert(split_str.0, split_str.1.to_string());
     }
-    else{
-        args.collect()
-    };
+    return key_map;
 }
 
 fn main() {
-   let args : Vec<String> =  cli_args(env::args());
-   let key_map = 
-    println!("{}", fs::read_to_string(&args[1]).expect("Unable to read file"));
+    let file_path : String = ality_args::get_file_path(env::args());
+    let file_str: String = ality_args::get_file_string(file_path);
+    let lines: Vec<&str> = file_str.lines().collect();
+    let keys_vec: Vec<&str> = match lines.iter().position(|&line| line.contains('-')) {
+        Some(index) => lines[..=index - 1].to_vec(),
+        None => lines.to_vec(),
+    };
+    let keys: HashMap<char, String> = get_keys(keys_vec);
 }
