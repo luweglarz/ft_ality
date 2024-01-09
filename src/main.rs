586d91b5dd9a644a7c819c63b71b1e04bed87598
RolandGamos <lucasweglarz9@gmail.com>
2024-01-09 10:32:00 +0100

    more functional

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,6 @@ use std::env;
 use std::collections::HashMap;
 
 fn main() {
-    let file_str: String = args::get_file_string(args::get_file_path(env::args()));
-    let lines: Vec<&str> = file_str.lines().collect();
-    let keys_vec: Vec<&str> = match lines.iter().position(|&line| line.contains('-')) {
-        Some(index) => lines[..=index - 1].to_vec(),
-        None => lines.to_vec(),
-    };
-    let keys: HashMap<char, String> = parsing::get_keys(keys_vec); 
-    let states_vec: Vec<&str> = match lines.iter().position(|&line| line.contains('-')) {
-        Some(index) => lines[1 + index..].to_vec(),
-        None => lines.to_vec(),
-    };
-    let states: HashMap<String, Vec<String>> = parsing::get_states(states_vec);
+    let keys: HashMap<char, String> = parsing::get_keys(args::get_file_string(args::get_file_path(env::args()))); 
+    let states: HashMap<String, Vec<String>> = parsing::get_states(args::get_file_string(args::get_file_path(env::args())));
 }
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,8 +1,16 @@
 use std::collections::HashMap;
 
-pub fn get_keys(keys_vec: Vec<&str>) -> HashMap<char, String>{
-    keys_vec.iter()
-            .filter_map(|&str| {
+fn get_keys_vec(file_str: String) -> Vec<String>{
+    let lines: Vec<&str> = file_str.lines().collect();
+    match lines.iter().position(|&line| line.contains('-')) {
+        Some(index) => lines[..=index - 1].iter().map(|&s| s.to_string()).collect(),
+        None => lines.iter().map(|&s| s.to_string()).collect(),
+    }
+}
+
+pub fn get_keys(file_str: String) -> HashMap<char, String>{
+    get_keys_vec(file_str).iter()
+            .filter_map(|str| {
                 let splitted: Vec<&str> = str.split("=").into_iter().map(|str| str.trim()).collect();
                 Some((
                     splitted.get(0)?.chars().next()?,
@@ -12,9 +20,17 @@ pub fn get_keys(keys_vec: Vec<&str>) -> HashMap<char, String>{
             .collect()
 }
 
-pub fn get_states (states_vec: Vec<&str>) -> HashMap<String, Vec<String>>{
-    states_vec.iter()
-            .filter_map(|&str| {
+fn get_states_vec(file_str: String) -> Vec<String>{
+    let lines: Vec<&str> = file_str.lines().collect();
+    match lines.iter().position(|&line| line.contains('-')) {
+        Some(index) => lines[1 + index..].iter().map(|&s| s.to_string()).collect(),
+        None => lines.iter().map(|&s| s.to_string()).collect(),
+    }
+}
+
+pub fn get_states(file_str: String) -> HashMap<String, Vec<String>>{
+    get_states_vec(file_str).iter()
+            .filter_map(|str| {
                 let splitted: Vec<&str> = str.split("=").into_iter().map(|str| str.trim()).collect();
                 Some((
                     splitted.get(1)?.to_string(),
